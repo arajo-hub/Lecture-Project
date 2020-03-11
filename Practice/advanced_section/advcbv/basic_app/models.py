e5f0f59067eea84d5cdd88fc89aaa38ee36e1445
arajo-hub <joara9566@naver.com>
2020-03-11 21:16:07 +0900

    Final commit

--- Practice/advanced_section/advcbv/basic_app/models.py
+++ Practice/advanced_section/advcbv/basic_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
 class School(models.Model):
     name=models.CharField(max_length=256)
     principal=models.CharField(max_length=256)
